1518be4637d4e0a4c3206c0c2e6d3bf1696c7553
Ben Van Citters <van.citters.b@gmail.com>
2013-10-13 15:40:00 -0700

    hgjk

--- of_v0.8.0_osx_release/apps/myApps/example-flickr/src/TileLayer.cpp
+++ of_v0.8.0_osx_release/apps/myApps/example-flickr/src/TileLayer.cpp
@@ -7,7 +7,6 @@
 //
 
 #include "TileLayer.h"
-#include "ofxJSONElement.h"
 //
 //#include "threadedObject.h"
 //#include "JSONThread1.h"
@@ -58,106 +57,28 @@ void TileLayer::update()
     
 }
 
-void TileLayer::testURL()
-{
-    ofxJSONElement  response;
-    int currentSize = 0;
-    
-    std::vector<std::string> imgUrlVector;
-
-    std::string jsonURL = "http://ec2-54-212-34-135.us-west-2.compute.amazonaws.com/recent";
-
-    if (!response.open(jsonURL)) {
-        cout  << "Failed to parse JSON\n" << endl;
-    }
-    
-    int fields  =response.size();
-    if(fields < 1)
-    {
-       ofLog(OF_LOG_ERROR, response.getRawString());
-    }
-    
-    for(int i=0; i< fields; i++)
-    {
-        std::string content  = response[i]["content"].asString();
-        std::string real_name  = response[i]["real_name"].asString();
-        std::string screen_name  = response[i]["screen_name"].asString();
-        uint64 timestamp  = response[i]["timestamp"].asUInt64();
-        
-        int imageCount = response[i]["images"].size();
-//        cout << "content: " << content << endl << " real_name: " << real_name << endl << " screen_name: " << screen_name << endl << " timestamp: " << timestamp << endl;
-        for(int j=0; j< imageCount; j++)
-        {
-            std::string url  = response[i]["images"][j].asString();
-            
-            cout << url << endl;
-            
-            DiamondTile tile( ofVec3f(ofRandom(ofGetWindowWidth()),ofRandom(ofGetWindowWidth())),
-                             ofVec2f(ofRandom(500),ofRandom(500)),
-                             ofVec2f(ofRandom(700),ofRandom(700)));
-            mTiles.push_back(tile);
-            imgUrlVector.push_back(url);
-        }
-    }
-    
-    for(int i = 0; i < imgUrlVector.size(); i++)
-    {
-        mLoader.loadFromURL(mTiles[i].mImage, imgUrlVector[i]);
-        mTiles[i].buildDiamondMesh();
-    }
-}
 
 
 void TileLayer::updateImages(int queries, bool refresh)
 {
-    ofxJSONElement response;
+//    ofxJSONElement response;
 //    int currentSize = 0;
     
     std::vector<std::string> imgUrlVector;
     for(int j = 0; j < queries; j++)
     {
-        int randomYear = 2004+(int)ofRandom(10);
-        int randomDay = 1+(int)ofRandom(28);
-        int randomMonth = 1+(int)ofRandom(12);
-        // 	  std::string url = "http://www.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=76fee119f6a01912ef7d32cbedc761bb&format=json&nojsoncallback=1&date=";
-        std::string jsonURL = "http://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=76fee119f6a01912ef7d32cbedc761bb&format=json&nojsoncallback=1&date=";
-        
-        std::stringstream sstm;
-        sstm <<jsonURL << randomYear << "-";
-        if(randomMonth < 10)
-            sstm << "0";
-        sstm <<  randomMonth << "-";
-        if(randomDay < 10)
-            sstm <<  "0";
-        sstm <<  randomDay;
-        if (!response.open(sstm.str()))
-        {
-            cout  << "Failed to parse JSON\n" << endl;
-        }
-        
-        int numImages =response["photos"]["photo"].size();        
-        
-        if(numImages < 1)
-            ofLog(OF_LOG_NOTICE, response.getRawString());
+        int numImages =0;
         
         ofLog(OF_LOG_NOTICE, "numImages %d",numImages);
         for(int i=0; i< numImages; i++)
         {
-            int farm = response["photos"]["photo"][i]["farm"].asInt();
-            std::string id = response["photos"]["photo"][i]["id"].asString();
-            std::string secret = response["photos"]["photo"][i]["secret"].asString();
-            std::string server = response["photos"]["photo"][i]["server"].asString();
-            std::string url = "http://farm"+ofToString(farm)+".static.flickr.com/"+server+"/"+id+"_"+secret+".jpg";
-            //            ofLog(OF_LOG_NOTICE, "url " + url);
-            imgUrlVector.push_back(url);
+            std::string url = "sdkjhfkaj";
+
             DiamondTile tile( ofVec3f(ofRandom(ofGetWindowWidth()),ofRandom(ofGetWindowWidth())),
                               ofVec2f(ofRandom(500),ofRandom(500)),
                               ofVec2f(ofRandom(700),ofRandom(700)));
-            if(!refresh)
-            {
-                mTiles.push_back(tile);
-                
-            }
+            
+            mTiles.push_back(tile);
             //            ofLog(OF_LOG_NOTICE,"images[%d]: %p",(i+currentSize),&images[i+currentSize]);
             //        string fileName = "snapshot_"+ofToString(10000+i)+".png";
             //		img.saveImage(fileName);
@@ -178,11 +99,6 @@ void TileLayer::updateImages(int queries, bool refresh)
             mTiles[i].buildDiamondMesh();
 //        }
     }
-}
-
-void TileLayer::loadImages(std::vector<std::string> imgUrlVector)
-{
-
 }
 
 void TileLayer::setMouseXY(int x, int y)
